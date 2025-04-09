ece05390ae42dbd97a3c46a94e58f8c7dbc75c04
ryanmongvn <ryan.mong.vn@gmail.com>
2025-04-10 02:59:12 +0700

    Update run-myapp.bat

--- run-myapp.bat
+++ run-myapp.bat
@@ -1,11 +1,8 @@
 @echo off
 set JAR_NAME=myapp.jar
 set LOG_FILE=app.log
+set PID_FILE=app.pid
 
-echo Killing old Java process...
-for /f "tokens=2 delims=," %%i in ('tasklist /FI "IMAGENAME eq java.exe" /FO CSV /NH') do (
-    taskkill /PID %%i /F >nul 2>&1
-)
-
-echo Starting %JAR_NAME%
-start /B java -jar %JAR_NAME% > %LOG_FILE% 2>&1
+echo Starting app...
+start /B cmd /c "java -jar %JAR_NAME% > %LOG_FILE% 2>&1 & echo ! > %PID_FILE%"
+echo App started.
