a79a335da3d8bb2c6e9222c5d2d26ffc96d8ecf8
ryanmongvn <ryan.mong.vn@gmail.com>
2025-04-10 03:08:02 +0700

    Update run-myapp.bat

--- run-myapp.bat
+++ run-myapp.bat
@@ -1,10 +1,24 @@
 @echo off
+setlocal EnableDelayedExpansion
+
 set JAR_NAME=myapp.jar
 set LOG_FILE=app.log
 set PID_FILE=app.pid
 
 echo Starting app...
 
-powershell -Command "Start-Process 'java' '-jar %JAR_NAME%' -NoNewWindow -PassThru | Select-Object -ExpandProperty Id > '%PID_FILE%'"
+:: Chạy hoàn toàn detached khỏi Jenkins shell
+start "" /B cmd /c "java -jar %JAR_NAME% > %LOG_FILE% 2>&1"
 
-echo App started with PID in %PID_FILE%.
+:: Đợi tiến trình được khởi tạo
+timeout /t 2 >nul
+
+:: Lấy PID java mới nhất (nếu chỉ có 1 app java đang chạy thì OK)
+for /f "tokens=2" %%i in ('tasklist /FI "IMAGENAME eq java.exe" /FO CSV /NH') do (
+    echo %%i > %PID_FILE%
+    goto Done
+)
+
+:Done
+echo PID saved to %PID_FILE%
+endlocal
