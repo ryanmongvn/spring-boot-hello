85a628593fb08efcf2105afc520273cb4f49a676
ryanmongvn <ryan.mong.vn@gmail.com>
2025-04-10 03:14:46 +0700

    Update run-myapp.bat

--- run-myapp.bat
+++ run-myapp.bat
@@ -1,29 +1,18 @@
 @echo off
-setlocal EnableDelayedExpansion
-
 set JAR_NAME=myapp.jar
-set LOG_FILE=app.log
 set PID_FILE=app.pid
 
-echo Starting app...
+:: Chạy app detached bằng cách gọi lại cmd
+start "" cmd /c "start /min java -jar %JAR_NAME%"
 
-:: Ghi lệnh vào 1 file tạm để tránh lỗi redirect
-echo @echo off > run-temp.bat
-echo java -jar !JAR_NAME! ^> !LOG_FILE! 2^>^&1 >> run-temp.bat
+:: Chờ 2 giây cho tiến trình khởi chạy
+ping -n 3 127.0.0.1 > nul
 
-:: Chạy tiến trình detached khỏi Jenkins
-start "" /min cmd /c run-temp.bat
-
-:: Chờ tiến trình khởi động
-timeout /t 2 >nul
-
-:: Ghi PID mới nhất của java (chỉ nếu có 1 tiến trình đang chạy)
+:: Lưu PID mới nhất của java
 for /f "tokens=2" %%i in ('tasklist /FI "IMAGENAME eq java.exe" /FO CSV /NH') do (
-    echo %%i > !PID_FILE!
+    echo %%i > %PID_FILE%
     goto Done
 )
 
 :Done
-echo App started with PID in !PID_FILE!
-
-endlocal
+echo App started and detached.
